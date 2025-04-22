f956d6e024050240ac13f24c880741df965e1c79
ravichandrapalla <106540626+ravichandrapalla@users.noreply.github.com>
2025-04-22 11:52:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mangoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const approutes = require("./routes/routes");
+const { default: mongoose } = require("mongoose");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/", approutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log("connected to mongoose"));
+  })
+  .catch((err) => console.log(err));
